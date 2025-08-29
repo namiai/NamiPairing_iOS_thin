694931943cdbde2277eed59bd160c08cca564615
gleb <gleb@glebs-Virtual-Machine.local>
2025-08-29 18:00:25 +0800

    release 4.1.0

--- Package@swift-5.10.swift
+++ Package@swift-5.10.swift
@@ -22,10 +22,10 @@ let package = Package(
         .package(url: "https://github.com/namiai/vgsl.git", from: "7.4.2")
     ],
     targets: [
-        .binaryTarget(name: "NamiPairingFramework", url: "https://github.com/namiai/NamiPairing_iOS_thin/releases/download/4.0.5/NamiPairingFramework.xcframework.zip",
-            checksum: "b78c350454689f7a96521937e683fc1f73ee46ab74450e9ecb5c164439fd042a"),
-        .binaryTarget(name: "StandardPairingUI", url: "https://github.com/namiai/NamiPairing_iOS_thin/releases/download/4.0.5/StandardPairingUI.xcframework.zip",
-            checksum: "bf0cf3657bc0e95746ec284686a5b559529b2d32e31939c993d05d7f2b7204d4"),
+        .binaryTarget(name: "NamiPairingFramework", url: "https://github.com/namiai/NamiPairing_iOS_thin/releases/download/4.1.0/NamiPairingFramework.xcframework.zip",
+            checksum: "b49d3cf0f9ce1cf6ea03f2f7f21df8a2f824e68b46f5bc045a4e1e48249f7fe0"),
+        .binaryTarget(name: "StandardPairingUI", url: "https://github.com/namiai/NamiPairing_iOS_thin/releases/download/4.1.0/StandardPairingUI.xcframework.zip",
+            checksum: "eae355d05e9f09fb7491ece3419c3140ef5bb4d39f9bdfb43c2aee58378bd239"),
         .target(
           name: "NamiPairingCore",
           dependencies: [
